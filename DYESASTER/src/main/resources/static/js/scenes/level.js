73f017ca8e7f28e97b8c9f890b0b188928bfa178
SevenWordsMusic <55382030+SevenWordsMusic@users.noreply.github.com>
2019-10-26 18:18:22 +0200

    Versión Alpha 0.00
    Multijugador en red semifuncional desde el menú, código de partida en consola del servidor.
    Cambio en el paradigma de ramas, a partir de ahora todos los commit se realizan sobre MASTER, para trabajar se creará una BRANCH desde MASTER y después está se volverá fusionará sobre MASTER de nuevo.

---
+++ DYESASTER/src/main/resources/static/js/scenes/level.js
@@ -0,0 +1,250 @@
+var LevelScene = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function LevelScene ()
+    {
+        Phaser.Scene.call(this, { key: 'levelScene', active: false });
+
+    },
+
+    preload: function()
+    {
+        var FKey = this.input.keyboard.addKey('F');
+        FKey.on('down', function () {
+            if (this.scale.isFullscreen)
+            {
+                this.scale.stopFullscreen();
+            }
+            else
+            {
+                this.scale.startFullscreen();
+            }
+        }, this);
+        
+        
+		this.load.scenePlugin('AnimatedTiles', 'AnimatedTiles.js', 'animatedTiles', 'animatedTiles');
+        
+		cursors1 = this.input.keyboard.addKeys({ 'up': Phaser.Input.Keyboard.KeyCodes.UP, 'left': Phaser.Input.Keyboard.KeyCodes.LEFT, 'right': Phaser.Input.Keyboard.KeyCodes.RIGHT, 'shoot': Phaser.Input.Keyboard.KeyCodes.CONTROL });
+		cursors2 = this.input.keyboard.addKeys({ 'up': Phaser.Input.Keyboard.KeyCodes.W, 'left': Phaser.Input.Keyboard.KeyCodes.A, 'right': Phaser.Input.Keyboard.KeyCodes.D, 'shoot': Phaser.Input.Keyboard.KeyCodes.SHIFT });
+    },
+    
+    create: function ()
+    {	 
+		this.bg_0 = this.add.tileSprite(0, 0, game.config.width, game.config.height, "bg-0");
+		this.bg_0.setOrigin(0, 0);
+		this.bg_0.setScrollFactor(0);
+		
+		// create an tiled sprite with the size of our game screen
+		this.bg_1 = this.add.tileSprite(0, 0, game.config.width, game.config.height, "bg-1");
+		// Set its pivot to the top left corner
+		this.bg_1.setOrigin(0, 0);
+		// fix it so it won't move when the camera moves.
+		// Instead we are moving its texture on the update
+		this.bg_1.setScrollFactor(0);
+
+		// Add a second background layer. Repeat as in bg_1
+		this.bg_2 = this.add.tileSprite(0, 0, game.config.width, game.config.height, "bg-2");
+		this.bg_2.setOrigin(0, 0);
+		this.bg_2.setScrollFactor(0);
+		
+		this.bg_3 = this.add.tileSprite(0, 0, game.config.width, game.config.height, "bg-3");
+		this.bg_3.setOrigin(0, 0);
+		this.bg_3.setScrollFactor(0);
+		
+		this.bg_4 = this.add.tileSprite(0, 0, game.config.width, game.config.height, "bg-4");
+		this.bg_4.setOrigin(0, 0);
+		this.bg_4.setScrollFactor(0);
+		
+		this.bg_5 = this.add.tileSprite(0, 0, game.config.width, game.config.height, "bg-5");
+		this.bg_5.setOrigin(0, 0);
+		this.bg_5.setScrollFactor(0);
+		
+		// load the map 
+		map = this.make.tilemap({key: 'map'});
+		
+		// tiles for the ground layer
+		allTiles = map.addTilesetImage('tiles');
+		// create the ground layer
+		groundLayer = map.createDynamicLayer('World', allTiles, 0, 0);
+		// the player will collide with this layer
+		groundLayer.setCollisionByExclusion([-1]);
+		
+		
+		blackHoleLayer = map.createDynamicLayer('BlackHole', allTiles, 0, 0);
+		blackHoleLayer.setCollisionByExclusion([-1]);
+		blackHoleLayer.setPosition(0, 96);
+
+    	//this.animatedTiles.init(map);
+		
+		// coin image used as tileset
+		//var coinTiles = map.addTilesetImage('coin');
+		// add coins as tiles
+		//coinLayer = map.createDynamicLayer('Coins', coinTiles, 0, 0);
+
+		// set the boundaries of our game world
+		this.physics.world.bounds.width = groundLayer.width;
+		this.physics.world.bounds.height = groundLayer.height;
+
+		// create the player sprite    
+		player1 = this.physics.add.sprite(200, 8688, 'player1');
+		player1.setBounce(0.2); // our player will bounce from items
+		player1.setCollideWorldBounds(true); // don't go out of the map
+		
+		player2 = this.physics.add.sprite(600, 8688, 'player2');
+		player2.setBounce(0.2); // our player will bounce from items
+		player2.setCollideWorldBounds(true); // don't go out of the map
+		
+		
+		// small fix to our player images, we resize the physics body object slightly
+		player1.body.setSize(player1.width, player1.height-8);
+		
+		// player will collide with the level tiles 
+		this.physics.add.collider(groundLayer, player1);
+		
+		// small fix to our player images, we resize the physics body object slightly
+		player2.body.setSize(player2.width, player2.height-8);
+		this.physics.add.collider(groundLayer, player2);
+		
+		this.physics.add.collider(player1, player2);
+		this.physics.add.collider(player2, player1);
+		
+		blackHoleLayer.setTileIndexCallback(211, deathByBlackHole, this);
+		this.physics.add.overlap(player1, blackHoleLayer);
+		this.physics.add.overlap(player2, blackHoleLayer);
+		
+		// player walk animation
+		this.anims.create({
+			key: 'walk1',
+			frames: this.anims.generateFrameNames('player1', {prefix: 'p1_walk', start: 1, end: 11, zeroPad: 2}),
+			frameRate: 10,
+			repeat: -1
+		});
+		// idle with only one frame, so repeat is not neaded
+		this.anims.create({
+			key: 'idle1',
+			frames: [{key: 'player1', frame: 'p1_stand'}],
+			frameRate: 10,
+		});
+		
+		
+		// player walk animation
+		this.anims.create({
+			key: 'walk2',
+			frames: this.anims.generateFrameNames('player2', {prefix: 'p1_walk', start: 1, end: 11, zeroPad: 2}),
+			frameRate: 10,
+			repeat: -1
+		});
+		// idle with only one frame, so repeat is not neaded
+		this.anims.create({
+			key: 'idle2',
+			frames: [{key: 'player2', frame: 'p1_stand'}],
+			frameRate: 10,
+		});
+	
+
+		this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+		this.cameraDolly = new Phaser.Geom.Point(player1.x, player1.y);
+		this.cameras.main.startFollow(this.cameraDolly);
+
+		// this text will show the score
+		text = this.add.text(20, 570, '0', {
+			fontSize: '20px',
+			fill: '#ffffff'
+		});
+		// fix the text to the camera
+		text.setScrollFactor(0);
+		
+    },
+	
+    update: function (time, delta) {   	
+        this.cameraDolly.x = Math.floor(player1.x);
+        this.cameraDolly.y = Math.floor(player1.y);
+    	
+    	blackHoleLayer.y= game.global.blackHolePosition;
+    	this.physics.world.bounds.height = groundLayer.height - 96 + game.global.blackHolePosition;
+    	this.cameras.main.setBounds(0, 0, map.widthInPixels, this.physics.world.bounds.height);
+    	
+    	
+    	if(player1.body){
+			if (cursors1.left.isDown)
+			{
+				player1.body.setVelocityX(-200);
+				player1.anims.play('walk1', true); // walk left
+				player1.flipX = true; // flip the sprite to the left
+			}
+			else if (cursors1.right.isDown)
+			{
+				player1.body.setVelocityX(200);
+				player1.anims.play('walk1', true);
+				player1.flipX = false; // use the original sprite looking to the right
+			} else {
+				player1.body.setVelocityX(0);
+				player1.anims.play('idle1', true);
+			}
+			// jump 
+			if (cursors1.up.isDown && player1.body.onFloor())
+			{
+				player1.body.setVelocityY(-500);        
+			}
+			//FIRE
+			if (cursors1.shoot.isDown )
+			{
+				
+			}
+    	}
+    	
+    	if(player2.body){
+			if (cursors2.left.isDown)
+			{
+				player2.body.setVelocityX(-200);
+				player2.anims.play('walk2', true); // walk left
+				player2.flipX = true; // flip the sprite to the left
+			}
+			else if (cursors2.right.isDown)
+			{
+				player2.body.setVelocityX(200);
+				player2.anims.play('walk2', true);
+				player2.flipX = false; // use the original sprite looking to the right
+			} else {
+				player2.body.setVelocityX(0);
+				player2.anims.play('idle2', true);
+			}
+			// jump 
+			if (cursors2.up.isDown && player2.body.onFloor())
+			{
+				player2.body.setVelocityY(-500);        
+			}
+			//FIRE
+			if (cursors2.shoot.isDown )
+			{
+	
+			}
+    	}
+		
+		// scroll the texture of the tilesprites proportionally to the camera scroll
+		this.bg_1.tilePositionX = this.cameras.main.scrollX * .2;
+		this.bg_2.tilePositionX = this.cameras.main.scrollX * .4;
+		this.bg_3.tilePositionX = this.cameras.main.scrollX * .6;
+		this.bg_4.tilePositionX = this.cameras.main.scrollX * .8;
+		
+		this.bg_1.tilePositionY = this.cameras.main.scrollY * .2;
+		this.bg_2.tilePositionY = this.cameras.main.scrollY * .4;
+		this.bg_3.tilePositionY = this.cameras.main.scrollY * .6;
+		this.bg_4.tilePositionY = this.cameras.main.scrollY * .8;
+		
+	}
+
+});
+
+
+function deathByBlackHole(sprite, tile) {
+	console.log(sprite);
+    sprite.destroy();
+    return false;
+}
+var shootTime= 0;
+var bullets;
+var groundLayer, blackHoleLayer, coinLayer;
